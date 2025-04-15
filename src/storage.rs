7b8ee0c4c971989b2439806ccecd7eea3c7158d8
Victor Kilyungi <victorkilyungi@gmail.com>
2025-04-15 12:23:20 +0300

    chore: Standardize Result type in storage functions

--- src/storage.rs
+++ src/storage.rs
@@ -2,13 +2,13 @@ use std::io;
 
 use crate::models::Task;
 
-pub fn save_to_json(tasks: &[Task]) -> io::Result<bool> {
+pub fn save_to_json(tasks: &[Task]) -> Result<bool, io::Error> {
     let json = serde_json::to_string_pretty(tasks)?;
     std::fs::write("tasks.json", json)?;
     Ok(true)
 }
 
-pub fn load_from_json(path: &str) -> io::Result<Vec<Task>> {
+pub fn load_from_json(path: &str) -> Result<Vec<Task>, io::Error> {
     let json = std::fs::read_to_string(path)?;
     let tasks = serde_json::from_str(&json)?;
     Ok(tasks)
